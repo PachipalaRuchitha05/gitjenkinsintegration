1b6ba293944b8646ac8d1a2f7d517a3f7dd43e2d
PachipalaRuchitha05 <rruchitha05@gmail.com>
2025-08-25 13:30:34 +0530

    upadated message in Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
+        System.out.println("New message is added!");
         
     }
 }
